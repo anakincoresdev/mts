5440e8f6c2df86a9c65a7a3da86da41a222d1786
anakincoresdev <anakincoresdev@gmail.com>
2020-05-09 21:05:05 +0700

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="home">
+    <app-button>
+      Кнопка
+    </app-button>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import AppButton from '@/components/ui/app-button.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    AppButton,
+  }
+}
+</script>
+
+<style lang="scss">
+  .home {
+    padding: 30px;
+    width: 100%;
+  }
+</style>
